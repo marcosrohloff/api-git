a95b4a59148064f800744c567f8e01a44be7683f
Marcos Fernando <marcos.fersh@gmail.com>
2025-08-28 01:09:13 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/apigit/ApiGitApplication.java
@@ -0,0 +1,13 @@
+package br.com.apigit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/apigit/ApiGitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apigit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
